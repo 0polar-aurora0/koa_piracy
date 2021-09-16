70c7aad3f16c154d65f9dcc5a7bfadf7e71c9803
0polar-aurora0 <1045612802@qq.com>
2021-09-16 17:22:50 +0800

    test

--- application.js
+++ application.js
@@ -1,12 +1,14 @@
 /*
  * @Author: fuzhenghao
  * @Date: 2021-09-16 17:08:17
- * @LastEditTime: 2021-09-16 17:08:18
+ * @LastEditTime: 2021-09-16 17:22:40
  * @LastEditors: fuzhenghao
- * @Description: 
+ * @Description:
  * @FilePath: \koa_piracy\application.js
- * 
+ *
  */
 class Application {
-    
+  aaa(params) {
+    console.log(aaa);
+  }
 }
